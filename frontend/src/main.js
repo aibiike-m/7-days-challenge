5087ea3dc8babf12fe59bd650a80673754c060c6
Bikoo0 <aibimadalieva06@gmail.com>
2026-01-02 22:27:49 +0600

    feat(i18n): add vue-i18n with en/ru locales

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,19 +1,11 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
+import i18n from './i18n'
 import '@/styles/main.scss'
-import axios from 'axios'
-
-axios.defaults.baseURL = 'http://127.0.0.1:8000'
-
-axios.interceptors.request.use(config => {
-	const token = localStorage.getItem('access_token')
-	if (token) {
-		config.headers.Authorization = `Bearer ${token}`
-	}
-	return config
-})
 
 const app = createApp(App)
+
 app.use(router)
+app.use(i18n)
 app.mount('#app')
