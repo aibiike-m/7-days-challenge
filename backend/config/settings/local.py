8677dd0e7eafdb3f0db694fbf7888a4946c9d3d9
Bikoo0 <aibimadalieva06@gmail.com>
2026-03-28 21:35:53 +0600

    chore(config): switch local email backend to console

--- backend/config/settings/local.py
+++ backend/config/settings/local.py
@@ -46,12 +46,7 @@ REST_FRAMEWORK = {
 }
 
 # Email
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 DEFAULT_FROM_EMAIL = f"SevenSteps <{env('EMAIL_HOST_USER')}>"
 
 # CACHES
